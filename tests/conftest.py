89d1db364416224f6c48cf57488692c4b242c577
Tommy Beadle <tbeadle@secureworks.com>
2024-01-19 15:21:22 -0500

    Update tests to work with updated database mechanics.
    - Use fixtures appropriately (i.e. be more pytest-y)
    - Honor the updated transaction handling. This generally consists of
      wrapping the API call in a transaction as it would be during a web
      request or an iteration of the scheduler. Allow that transaction to be
      committed to make sure rows are written to the database correctly. Use
      another transaction to do validation that the database was updated as
      expected. We need to wrap those in a transaction so that it doesn't
      create an implicit one upon the first database command sent to it.
    - Enable engine.echo so that, upon test failure or if `-s` is supplied
      to `pytest`, the SQL statements issued to the database are output.
      This enables for manual verification that it's issuing the commands we
      expect it to.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,19 @@
 import pytest
 
 import lib.cuckoo.core.database
+from lib.cuckoo.core.database import Database, init_database, reset_database
+
+
+@pytest.fixture
+def db():
+    reset_database()
+    try:
+        init_database(dsn="sqlite://")
+        retval = Database()
+        retval.engine.echo = True
+        yield retval
+    finally:
+        reset_database()
 
 
 @pytest.fixture
